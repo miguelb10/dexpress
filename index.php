8a394ac9548d63f5af38cc7bd51a803fead27a03
miguelb10 <miguel.blas_03@hotmail.com>
2020-10-21 03:02:14 -0500

    first commit
    base

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>DExpress</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <section id="topbar" class="d-none d-lg-block">
+        <div class="container clearfix">
+            <div class="float-left">
+                <img style="max-width: 50%;" src="assets/img/logo-dexpresshf.png">
+            </div>
+            <div class="social-links float-right">
+                <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+                <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+                <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+                <a href="#" class="skype"><i class="icofont-skype"></i></a>
+                <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+            </div>
+        </div>
+    </section>
+    <header id="header">
+        <div class="container">
+            <div class="logo float-left logo-mobile">
+                <h1 class="text-light"><a href="index.html"><span>Mamba</span></a></h1>
+            </div>
+            <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                    <li class="active"><a href="index.html">Inicio</a></li>
+                    <li><a href="#about">Quiénes Somos</a></li>
+                    <li><a href="#services">Servicios</a></li>
+                    <li><a href="#portfolio">Contáctanos</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/counterup/counterup.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
